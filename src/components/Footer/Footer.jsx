15cd7f5827a38dc18cf2d9632ad175dada96bf89
ivi0540 <ivi0540@gmail.com>
2025-06-30 21:15:47 +0300

    feat: add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import "./style.scss";
+import { Link } from "react-router-dom";
+import { TMDBLogo } from "../../components/TMDBLogo/TMDBLogo";
+import GitHubLogo from "./images/github.png";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="footer">
+      <Link
+        to={"https://developer.themoviedb.org/reference/intro/getting-started"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <TMDBLogo />
+      </Link>
+
+      <div className="footer__item">© {currentYear}</div>
+
+      <Link
+        to={"https://github.com/ivi0540"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img className="footer__GitHubLogo" src={GitHubLogo} alt="GitHub Profile" />
+      </Link>
+    </footer>
+  );
+};
+
+export { Footer };
--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,14 +1,11 @@
 import { useState, useEffect } from "react";
-import "./style.scss";
-import GitHubLogo from "./images/github.png";
-import { Link } from "react-router-dom";
 import { Header } from "../../components/Header/Header";
+import { Footer } from "../../components/Footer/Footer";
 import { getNowPlayingMovies } from "../../api";
+import "./style.scss";
 
 const TestPage = () => {
   const [newestMovie, setNewestMovie] = useState(null);
-
-  const currentYear = new Date().getFullYear();
   const back = newestMovie?.results[0]?.backdrop_path
     ? {
         backgroundImage: `url(https://image.tmdb.org/t/p/w1280${newestMovie.results[0].backdrop_path})`,
@@ -26,34 +23,8 @@ const TestPage = () => {
   return (
     <>
       <Header />
-
       <main className="main" style={back}></main>
-
-      <footer className="footer">
-        <Link
-          to={
-            "https://developer.themoviedb.org/reference/intro/getting-started"
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {/* <img
-            className="header__logo"
-            src={TMDBLogo}
-            alt="The Movie Database Logo"
-          /> */}
-        </Link>
-
-        <div className="footer__item">© {currentYear}</div>
-
-        <Link
-          to={"https://github.com/ivi0540"}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="header__logo" src={GitHubLogo} alt="GitHub Profile" />
-        </Link>
-      </footer>
+      <Footer />
     </>
   );
 };
