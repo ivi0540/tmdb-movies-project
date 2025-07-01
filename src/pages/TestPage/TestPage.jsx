07ee05b0f294076ebbfae42b6001d76b0ede8926
ivi0540 <ivi0540@gmail.com>
2025-07-01 20:50:48 +0300

    feat: add newest-movie

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,29 +1,13 @@
-import { useState, useEffect } from "react";
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
-import { getNowPlayingMovies } from "../../api";
+import { NewestMovie } from "../../components/NewestMovie/NewestMovie";
 import "./style.scss";
 
 const TestPage = () => {
-  const [newestMovie, setNewestMovie] = useState(null);
-  const back = newestMovie?.results[0]?.backdrop_path
-    ? {
-        backgroundImage: `url(https://image.tmdb.org/t/p/w1280${newestMovie.results[0].backdrop_path})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-      }
-    : {
-        backgroundColor: "red",
-      };
-
-  useEffect(() => {
-    getNowPlayingMovies().then((data) => setNewestMovie(data));
-  }, []);
   return (
     <>
       <Header />
-      <main className="main" style={back}></main>
+      <NewestMovie />
       <Footer />
     </>
   );
