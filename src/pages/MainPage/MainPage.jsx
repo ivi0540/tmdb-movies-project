a0d00cfacdc627c13826b66984a5f92e40f0521c
ivi0540 <ivi0540@gmail.com>
2026-01-28 22:55:13 +0300

    feat: add global color variables

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,5 @@
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
-// import { NewestMovie } from "../../components/NewestMovie/NewestMovie";
 import { NewMoviesSlider } from "../../components/NewMoviesSlider/NewMoviesSlider";
 import "./style.scss";
 
@@ -8,7 +7,6 @@ const MainPage = () => {
   return (
     <>
       <Header />
-      {/* <NewestMovie /> */}
       <NewMoviesSlider />
       <Footer />
     </>
